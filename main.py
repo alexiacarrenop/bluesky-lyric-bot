159ae770ed58a4a4b9dd1c06a5860cf7eb870f6d
alexi <c3073816@newcastle.ac.uk>
2026-06-25 00:34:20 +0100

    Changed from config credentials to GitHub secrets

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
+import os
 import db_manager
-from config import client
-
+from atproto import Client
                      
 def job():
+    # from github secrets
+    username = os.eniron.get("BLUESKY_USERNAME")
+    password = os.environ.get("BLUESKY_PASSWORD")
+
+    if not username or not password:
+        print("Error: username and/or password not found in environment secrets.")
+        return
+    
+    #log in
+    client = Client()
+    client.login(username, password)
+
     quote = db_manager.get_random_quote()
     if quote:
         client.send_post(text=quote)
